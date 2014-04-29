0d7d8c2a3385fda62a4b04b6658b6eb53cfa37d6
CaptConrado <ottey001@gmail.com>
2014-04-29 15:33:17 -0700

    added the paypal stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429215724) do
+ActiveRecord::Schema.define(version: 20140429221435) do
 
   create_table "channels", force: true do |t|
     t.string   "cuid"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140429215724) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "payment_type"
+    t.string   "paypal_email"
   end
 
 end
