f703b7ecad8deb4fb16ee5dd8c0d8f11d78f1a87
CaptConrado <ottey001@gmail.com>
2014-05-01 11:46:00 -0700

    added the payments

--- app/models/assetgroup.rb
+++ app/models/assetgroup.rb
@@ -1,3 +1,4 @@
 class Assetgroup < ActiveRecord::Base
+  belongs_to :user
   has_many :payments, as: :payable
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ActiveRecord::Base
   belongs_to :payable, polymorphic: true
-  belongs_to :channel
-  belongs_to :assetgroup
+  # belongs_to :channel
+  # belongs_to :assetgroup
 end
